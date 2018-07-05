881b86ba1ae1ceb032b220d04704e45e062a26e2
Will Hay Jr <willhayjr@gmail.com>
2018-07-05 15:38:11 -0400

    revert deletion of editable item

--- src/components/components.ts
+++ src/components/components.ts
@@ -1,9 +1,11 @@
 import { ACTION_SHEET_COMPONENTS } from './action-sheets';
+import { EditableItemComponent } from './editable-item/editable-item';
 import { EXPANDABLE_HEADER_COMPONENTS } from './expandable-header/expandable-header';
 import { MINI_MODAL_COMPONENTS } from './mini-modal/mini-modal';
 
 export const COMPONENTS = [
   MINI_MODAL_COMPONENTS,
   EXPANDABLE_HEADER_COMPONENTS,
-  ACTION_SHEET_COMPONENTS
+  ACTION_SHEET_COMPONENTS,
+  EditableItemComponent
 ];
